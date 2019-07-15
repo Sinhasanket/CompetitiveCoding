e1ffc33aa3b1a8cb1edb3e9409f111acde6c8e26
sanketsinha <sinha.sanket99@gmail.com>
2019-07-16 00:30:44 +0530

    codevitasol

---
+++ Codevita/LexiString.java
@@ -0,0 +1,73 @@
+package Codevita;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class LexiString {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String a[]){
+        FastReader sc = new FastReader();
+        int test = sc.nextInt();
+        while(test-->0){
+            String P = sc.next();
+            String S = sc.next();
+            solve(P,S);
+        }
+    }
+
+    public static void solve(String p,String s){
+        int len = s.length();
+        char pchar[] = p.toCharArray();
+        char schar[] = s.toCharArray();
+        char ch[] = new char[len];
+        int z=0;
+        for(int i=0;i<p.length();i++){
+            for(int j=0;j<s.length();j++){
+                if(pchar[i]==schar[j]){
+                    ch[z] = pchar[i];
+                    z++;
+                }
+            }
+        }
+        String ans = new String(ch);
+        System.out.println(ans);
+    }
+}
+
+//input
+//2
+//polikujmnhytgbvfredcxswqaz
+//abcd
+//qwryupcsfoghjkldezxvbintma
+//ativedoc
+
+//output
+//bdca
+//codevita
---
+++ Codevita/SimilarChar.java
@@ -0,0 +1,63 @@
+package Codevita;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SimilarChar {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+    }
+
+    public static void main(String[] a) throws IOException {
+        try{
+           FastReader input = new FastReader();
+            int lenght_Of_String = input.nextInt();
+            String get_string = input.next();
+            char get_char[] = get_string.toCharArray();
+            int query = input.nextInt();
+            for(int j=0;j<query;j++){
+                int queries = input.nextInt();
+                int count=0;
+                for(int i=0;i<queries-1;i++){
+                    if(get_char[i]==get_char[queries-1]){
+                        count++;
+                    }
+                }
+                StringBuffer buffer = new StringBuffer();
+                buffer.append(count);
+                System.out.println(buffer);
+            }
+        }catch(Exception e){
+            return;
+        }
+    }
+}
